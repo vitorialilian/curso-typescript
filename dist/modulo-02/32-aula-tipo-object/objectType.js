c7ba41eba5ad0097193009834d2227fd11dfead6
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2024-08-17 13:12:49 -0300

    feat: atualização

--- dist/modulo-02/32-aula-tipo-object/objectType.js
+++ dist/modulo-02/32-aula-tipo-object/objectType.js
@@ -7,3 +7,74 @@ const pessoa = {
     funcao: 'Fullstack Developer'
 };
 console.log(pessoa);
+function onboarding01(funcionario) {
+    return 'Seja bem-vinda ' + funcionario.nome;
+}
+console.log(onboarding01({ nome: 'Vitória Lilian' }));
+function onboarding02(pessoa) {
+    return ('Seja bem-vinda ' +
+        pessoa.nome +
+        '!' +
+        ' Sua função aqui na empresa será ' +
+        pessoa.funcao +
+        '.');
+}
+console.log(onboarding02({ nome: 'Vitória Lilian', funcao: 'Frontend Developer' }));
+function onboarding03(pessoa) {
+    return ('Seja bem-vinda ' +
+        pessoa.nome +
+        '!' +
+        ' Sua função aqui na empresa será ' +
+        pessoa.funcao +
+        '.' +
+        ' Você trabalhará com a linguagem ' +
+        pessoa.linguagem +
+        '.');
+}
+console.log(onboarding03({ nome: 'Vitória Lilian', funcao: 'Frontend Developer', linguagem: 'TypeScript/JavaScript' }));
+function onboarding04(pessoa) {
+    return ('Seja bem-vinda ' +
+        pessoa.nome +
+        '!' +
+        ' Sua função aqui na empresa será ' +
+        pessoa.funcao +
+        '.' +
+        ' Você trabalhará com a linguagem ' +
+        pessoa.linguagem +
+        '.');
+}
+console.log(onboarding04({ nome: 'Vitória Lilian', funcao: 'Frontend Developer', linguagem: 'TypeScript/JavaScript' }));
+function onboarding05(pessoa) {
+    return ('Seja bem-vinda ' +
+        pessoa.nome +
+        '!' +
+        ' Sua função aqui na empresa será ' +
+        pessoa.funcao +
+        '.' +
+        ' Você trabalhará com a linguagem ' +
+        pessoa.linguagem +
+        '.' +
+        ' Seu e-mail será ' +
+        pessoa.email);
+}
+console.log(onboarding05({ nome: 'Vitória Lilian', funcao: 'Frontend Developer', linguagem: 'TypeScript/JavaScript', email: 'vitorialilian@microsoft.com' }));
+const filha = {
+    nome: 'Vitória',
+    sobrenome: 'Lilian',
+    idade: 27
+};
+console.log(filha);
+const usuario = {
+    nome: 'Vitória Lilian',
+    email: 'vl@gmail.com'
+};
+const admin = {
+    nome: 'Vitória Lilian',
+    email: 'vl@gmail.com',
+    admin: true
+};
+function acessarSistema(usuario) {
+    return usuario;
+}
+console.log(acessarSistema(usuario));
+console.log(acessarSistema(admin));
