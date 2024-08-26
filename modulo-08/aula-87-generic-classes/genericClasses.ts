94495739b846957aa553b572f116a3cd7d2e329f
vitorialilian <vitorialilian3096@gmail.com>
2024-08-26 13:17:41 -0300

    feat: aula generic classes modulo 08

--- modulo-08/aula-87-generic-classes/genericClasses.ts
+++ modulo-08/aula-87-generic-classes/genericClasses.ts
@@ -24,3 +24,26 @@ const boxNumber = new Box<number>(2);
 console.log(boxString.getItem()); // saída: Neve Usagi
 console.log(boxNumber.getItem()); // saída: 2
 
+// Exemplo 02 
+class Estudante<T, U> {
+    private id: T;
+    private nome: U;
+
+    setValor(id: T, nome: U): void { 
+        this.id = id;
+        this.nome = nome;
+    }
+
+    retornarValor(): void {
+        console.log(`Identificação do Estudante: ${this.id}, Nome do Estudante: ${this.nome}`);
+    }
+}
+
+const estudante = new Estudante<number, string>();
+const estudanteSecundario = new Estudante<string, string>();
+
+estudante.setValor(234, 'Vitória Lilian');
+estudante.retornarValor();
+
+estudanteSecundario.setValor('235', 'Jonas Alves');
+estudanteSecundario.retornarValor();
